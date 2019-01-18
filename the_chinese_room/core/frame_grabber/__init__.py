0a666db45339129e762750e56ced7f0f447338f8
wo1fsea <wo1fsea@126.com>
2019-01-18 14:05:53 +0800

    add: import FrameGrabber in frame_grabber

---
+++ the_chinese_room/core/frame_grabber/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""----------------------------------------------------------------------------
+Author:
+	Huang Quanyong
+	gzhuangquanyong@corp.netease.com
+Date:
+	2019/1/18
+Description:
+	__init__
+----------------------------------------------------------------------------"""
+
+from .frame_grabber import FrameGrabber
